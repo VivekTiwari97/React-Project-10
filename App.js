a56f3b0c8f2684aea135975622bb8e33e79d231d
vivek <vivektiwarivip2@gmail.com>
2023-07-13 13:45:27 +0530

    react  project 10
    react  project 10

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Web from './Website/Web';
+
+
+
+const App = () => {
+  return (
+    <div>
+     <Web/>
+    </div>
+  )
+};
+
+export default App;
---
+++ Website/Web.jsx
@@ -0,0 +1,54 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import "./web.css"
+
+const Web = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError('Error fetching data from the API');
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <Container className='box'> 
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} xs={12} sm={6} md={4}>
+            <Card>
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>${product.price}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Web;
